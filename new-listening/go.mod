d0ed0d5d1f32af70738567c0c5ae31db38723e64
anhkhoinp94 <anhkhoi.np94@gmail.com>
2025-02-10 19:45:08 +0700

    2025-02-10_0745 PM

---
+++ new-listening/go.mod
@@ -0,0 +1,3 @@
+module new-listening
+
+go 1.23.2
---
+++ new-listening/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+type Paragraph struct {
+	P string `json:"p"`
+}
+
+func main() {
+	file, err := os.Open("paragraphs.json")
+	if err != nil {
+		fmt.Println("Error opening JSON file:", err)
+		return
+	}
+	defer file.Close()
+
+	var paragraphs []Paragraph
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&paragraphs); err != nil {
+		fmt.Println("Error decoding JSON file:", err)
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(paragraphs), func(i, j int) {
+		paragraphs[i], paragraphs[j] = paragraphs[j], paragraphs[i]
+	})
+
+	clearScreen()
+
+	for i := 0; i < len(paragraphs); i++ {
+		text := paragraphs[i].P
+
+		sentences := strings.Split(text, ". ")
+		if len(sentences) > 0 && sentences[len(sentences)-1] == "" {
+			sentences = sentences[:len(sentences)-1] // Remove empty last element
+		}
+
+		// Speak each sentence 10 times
+		for _, sentence := range sentences {
+			sentence = strings.TrimSpace(sentence) + "." // Ensure it ends with a period
+			fmt.Println(sentence)
+			for j := 0; j < 10; j++ {
+				speak(sentence)
+				time.Sleep(250 * time.Millisecond)
+			}
+			clearScreen()
+		}
+
+		// Speak the whole paragraph 5 times
+		fmt.Println(text)
+		fmt.Printf("%v paragraphs left\n", len(paragraphs)-i-1)
+		for k := 0; k < 5; k++ {
+			speak(text)
+			time.Sleep(250 * time.Millisecond)
+		}
+		clearScreen()
+	}
+
+	fmt.Println("All paragraphs have been listened. Exiting.")
+}
+
+func clearScreen() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func speak(text string) {
+	cmd := exec.Command("tts", text)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error putting the system to sleep:", err)
+		return
+	}
+}
