32063d7de94e4917f52fd35c68ef5c613a853c16
anhkhoinp94 <anhkhoi.np94@gmail.com>
2025-02-19 08:40:44 +0700

    2025-02-19_0840 AM

--- listening/main.go
+++ listening/main.go
@@ -50,6 +50,7 @@ func main() {
 	fmt.Println("All paragraphs have been listened. Exiting.")
 }
 
+//lint:ignore U1000 This function is used to reverse slice
 func reverseSlice(slice []Paragraph) []Paragraph {
 	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
 		slice[i], slice[j] = slice[j], slice[i]
